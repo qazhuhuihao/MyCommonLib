34673b81af7a1f511063933908775afb5c6ab577
hhh <475780541@qq.com>
2018-02-27 23:24:48 +0800

    fix

--- sample/src/main/java/cn/hhh/mycommonlib/MyApplication.java
+++ sample/src/main/java/cn/hhh/mycommonlib/MyApplication.java
@@ -2,6 +2,7 @@ package cn.hhh.mycommonlib;
 
 import cn.hhh.commonlib.MyBaseApplication;
 import cn.hhh.commonlib.common.Configs;
+import cn.hhh.commonlib.utils.Logg;
 
 /**
  * @author qazhu
@@ -16,6 +17,19 @@ public class MyApplication extends MyBaseApplication {
 
         //栗子默认使用debug模式
         Configs.DEBUG = true;
+
+        if (Configs.DEBUG) {
+            Logg.LoggExample loggExample = new Logg.LoggExample();
+            boolean flag = false;
+            for (Logg.LoggInterface loggInterface : Logg.getLoggs()) {
+                if (loggInterface.getTAG().equals(loggExample.getTAG())) {
+                    flag = true;
+                    break;
+                }
+            }
+            if (!flag)
+                Logg.addLogg(loggExample);
+        }
     }
 
 }
