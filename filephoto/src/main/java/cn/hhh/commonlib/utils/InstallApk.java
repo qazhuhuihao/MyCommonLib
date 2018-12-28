90f1af50832c185fad5de1ec0a34b94c3e9aa5bf
hhh <475780541@qq.com>
2018-12-28 12:53:50 +0800

    修改获取图片,添加安装app方法,防止冲突

---
+++ filephoto/src/main/java/cn/hhh/commonlib/utils/InstallApk.java
@@ -0,0 +1,34 @@
+package cn.hhh.commonlib.utils;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v4.content.FileProvider;
+
+import java.io.File;
+
+public class InstallApk {
+
+    /**
+     * 安装apk
+     */
+    public void installApk(File file) {
+        Intent intent = new Intent();
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        //执行动作
+        intent.setAction(Intent.ACTION_VIEW);
+        //执行的数据类型
+
+        System.out.println(file.getAbsolutePath());
+
+        Uri apkUri;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            apkUri = FileProvider.getUriForFile(UIUtil.getContext(), UIUtil.getContext().getPackageName() + ".fileprovider", file);
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        } else {
+            apkUri = Uri.fromFile(file);
+        }
+        intent.setDataAndType(apkUri, "application/vnd.android.package-archive");
+        UIUtil.getContext().startActivity(intent);
+    }
+}
